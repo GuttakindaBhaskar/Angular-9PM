a64990690cd56f7f16dbd74021b8f8b3ba194d08
samba.pendela@gmail.com <samba.pendela@gmail.com>
2021-02-04 22:07:37 +0530

    code

--- miniproject/miniproject-app/src/app/services/products.service.ts
+++ miniproject/miniproject-app/src/app/services/products.service.ts
@@ -9,6 +9,6 @@ import { Product } from "../modal/product.modal";
 export class productService{
     constructor(private http:HttpClient){}
     getAllProducts():Observable<Product[]>{
-        return this.http.get<Product[]>(`http://localhost:8080/products`);
+        return this.http.get<Product[]>(`http://server9pm-env.eba-hipzi2st.ap-south-1.elasticbeanstalk.com/products`);
     }
 }
